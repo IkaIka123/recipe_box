2dbc8f7d2fb9817c02e245ff3ded8e1ec927cbdf
Ivana Katic <katic.ivana123@gmail.com>
2020-05-15 23:44:22 +0200

    added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
 
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
